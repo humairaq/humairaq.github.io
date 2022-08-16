1cb592ed8c5a9a5cb272d16f8996940744ce9f8d
Humaira Qadeer <humaira.qadeer29@qmail.cuny.edu>
2022-08-16 06:15:33 -0400

    updated index.html

--- js/main.js
+++ js/main.js
@@ -17,10 +17,31 @@ function image_toggle(project_name,project_content_name){
 
 
 function see_more_projects() {
-    let loginContent = document.querySelector('.more-projects')
-    if(loginContent.style.display===""){
-        loginContent.style.display="grid";
+    let projectContent = document.querySelector('.more-projects')
+    let seeMoreButton = document.getElementById('see-more')
+    if(projectContent.style.display===""){
+        projectContent.style.display="grid";
+        seeMoreButton.textContent= "close";
     } else {
-        loginContent.style.display="";
+        projectContent.style.display="";
+        seeMoreButton.textContent= "view all";
     }
 }
+
+$(document).ready(function(){
+
+    $(".project-tabs-list li a").click(function(e){
+       e.preventDefault();
+    });
+  
+    $(".project-tabs-list li").click(function(){
+       var tabid = $(this).find("a").attr("href");
+       $(".project-tabs-list li, .project-tabs .tab").removeClass("active");   // removing active class from tab
+  
+       $(".tab ").hide();   // hiding open tab
+       $(tabid).show();    // show tab
+       $(this).addClass("active"); //  adding active class to clicked tab
+  
+    });
+  
+  });
